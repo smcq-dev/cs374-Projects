a2e69abb18b80304ca9005e5f4d9238fe522bdf4
Sean McQuillan <mcquills@oregonstate.edu>
2026-01-18 21:07:28 -0800

    Working Recursion and directory printing.

--- 2/filescanner.c
+++ 2/filescanner.c
@@ -3,46 +3,57 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <string.h>
+
+int scan_directories(char *path);
 
 int main(int argc, char *argv[]) {
-
-    DIR *dir;
-    struct dirent *structd;
-    struct stat statbuf;
-    const char *path = (argc == 2) ? argv[1] : ".";
+    char *path = (argc == 2) ? argv[1] : ".";
 
     if (argc > 2) {
         fprintf(stderr, "usage: filescanner [directory]\n");
         exit(1);
     }
 
-    dir = opendir(path);
+    scan_directories(path);
+    return 0;
+}
+
+
+int scan_directories(char *path) {
+    struct dirent *structd;
+    struct stat statbuf;
+    int total_size = 0;
+    #define PATH_MAX_SIZE 1024
+    char fullpath[PATH_MAX_SIZE];
+
+    DIR *dir = opendir(path);
 
     if (dir == NULL) {
-        perror("Opening the directory is causing:");
+        perror("opendir");
         exit(1);
     }
 
-    int directory_size = 0;
-    char fullpath[1024];
-
     while ((structd = readdir(dir)) != NULL) {
-        printf("%s\n", structd->d_name);
+
+        if ((strcmp(structd->d_name,".") == 0) || (strcmp(structd->d_name,"..") == 0)) {
+            continue;
+        }
+
         snprintf(fullpath, sizeof(fullpath), "%s/%s", path, structd->d_name);
         lstat(fullpath, &statbuf);
 
         if (S_ISREG(statbuf.st_mode)) {
-            directory_size += statbuf.st_size;
+            total_size += statbuf.st_size;
+        }
+
+        if (S_ISDIR(statbuf.st_mode)) {
+            total_size += scan_directories(fullpath);
         }
         
     }
 
-    snprintf(fullpath, sizeof(fullpath), ": %d", directory_size);
-    printf("%s: %d\n", path, directory_size);
-        
-
     closedir(dir);
+    printf("%s: %d\n", path, total_size);
+    return total_size;
 }
-
-
-//char * search_directories(void){}
