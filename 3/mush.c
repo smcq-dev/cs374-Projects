4379271055df883b3d6e418af91e6e9c44c0d0a0
Sean McQuillan <mcquills@oregonstate.edu>
2026-01-23 20:08:22 -0800

    Words now stored in an array.

--- 3/mush.c
+++ 3/mush.c
@@ -5,22 +5,32 @@
 #include <sys/wait.h>
 
 #define COMMAND_LINE_MAX_SIZE 2048
+#define MAX_WORDS_COMMAND_LINE 128
 
 int main(int argc, char *argv[]) {
 
     char buffer[COMMAND_LINE_MAX_SIZE];
     char *token;
+    char *words[MAX_WORDS_COMMAND_LINE];
+   
 
     while(1) {
+        int counter = 0;
         printf("mush$ ");
         
         fgets(buffer, sizeof(buffer), stdin);
 
-        if ((token = strtok(buffer, " ")) != NULL) do {
+        if ((token = strtok(buffer, " \n")) != NULL) do {
         printf("%s\n", token);
+        words[counter] = token;
+        counter ++;
   
-        } while ((token = strtok(NULL, " ")) != NULL);
+        } while ((token = strtok(NULL, " \n")) != NULL);
 
-       
+        words[counter] = NULL;
+        for (int i = 0; words[i] != NULL; i++) {
+            printf("%s\n", words[i]);
+        }
+
     }
 }
